c47551fc1301e64ae3f9bdb6f04b2a696926822e
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-24 18:59:58 +0200

    Updated but not working correctly

--- management.php
+++ management.php
@@ -9,7 +9,16 @@
 </head>
 <body>
     <?php
-        // recupero dati prenotazione ristorante
+        const APPET_PRICE = 5;
+        const FIRST_PRICE = 6;
+        const SECOND_PRICE = 7;
+        const DISCOUNT_ALL = 0.15;
+        const DISCOUNT_FS = 0.1;
+        $available_waiters = ['Mario', 'Giuseppe', 'Luca', 'Andrea', 'Marco'];
+        
+        $total = 0; 
+        $act_discount = 0;
+        //! recupero dati prenotazione ristorante
         $name = $_POST['name'];
         $surname = $_POST['surname'];
         $table = $_POST['table_num'];
@@ -20,11 +29,59 @@
             $courses = $_POST['courses[]'];
             
         } else {
-            echo "Non puoi fare un'ordinazione vuota!";
-        }
-    ?> 
+            echo "<p> Non puoi fare un'ordinazione vuota! </p> <br> <p> ora prenotazione: $time </p> <br>";
+            echo "<a href='index.html' target='_self> torna alla prenotazione</a>";
+            exit();
+        } 
 
+        if(in_array("antipasto" , $courses) && !in_array("primo", $courses) && !in_array("secondo" , $courses)) {
+            echo "Non puoi ordinare solo un antipasto!";
+            echo "<a href='index.html' target='_self> torna alla prenotazione</a>";
+            exit();
+        }  elseif(in_array("antipasto" , $courses) && in_array("primo", $courses) &&  in_array("secondo" , $courses)) 
+            $act_discount = DISCOUNT_ALL;
+        elseif(!in_array("antipasto" , $courses) && in_array("primo", $courses) &&  in_array("secondo" , $courses)) 
+            $act_discount = DISCOUNT_FS;
+
+        $total = floatval(APPET_PRICE + FIRST_PRICE + SECOND_PRICE) * (1 - $act_discount);      //* Primo calcolo del totale
+
+        if($parking != "Niente parcheggio") {
+            $total += ($parking == "Parcheggio custodito") ? 5.0 : 3.0;                                //* Eventuale aggiunta causa uso del parcheggio a pagamento 
+        }
+
+        $waiter = $available_waiters[array_rand($available_waiters)];                           //* Scelta di un cameriere casuale
+    ?> 
     
+    <h1 class="my-2">Prenotazione ristorante</h1>
+    <div class="table_container w-50 mx-auto">
+        <table>
+            <tr>
+                <th>Cliente</th>
+                <th>Tavolo N°</th>
+                <th>Ora</th>
+                <th>Uso del parcheggio</th>
+                <th>Portate</th>
+                <th>Cameriere assegnato</th>
+                <th>sconto applicato</th>
+                <th>Note aggiuntive</th>
+                <th>Totale preventivo</th>
+
+            </tr>
+            <tr>
+                <td><?php echo "$name $surname" ?></td>           <!--nome e cognome sulla stessa cella della tabella-->
+                <td><?php echo $table  ?>           </td>
+                <td><?php echo $time  ?>            </td>
+                <td><?php echo $parking  ?>         </td>
+                <td><?php echo $courses  ?>         </td>
+                <td><?php echo $waiter  ?>          </td>
+                <td><?php echo "$act_discount%" ?>  </td>
+                <td><?php echo $notes ?>            </td>
+                <td><?php echo "$total €" ?>        </td>
+            </tr>
+        </table>
+    </div>
+    <p class="text-center"> TI ASPETTIAMO!   </p>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
